4510eff3f7de6781c5f5846cb0e283f35021ec1a
MichaelEldredge <michaelgeldredge@gmail.com>
2022-07-02 22:02:52 -0600

    Checks passwords and sets role according to database

--- src/main/java/com/revature/project0/Project0.java
+++ src/main/java/com/revature/project0/Project0.java
@@ -1,6 +1,11 @@
 package com.revature.project0;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
+import com.revature.jdbc.utils.ConnectionUtils;
 
 public class Project0 {
 
@@ -10,18 +15,34 @@ public class Project0 {
 
 	public static void login() {
 		System.out.println("Username: ");
-		Scanner sc = new Scanner(System.in);
-		
-		user = sc.nextLine();
-		System.out.println("Password:");
-		password = sc.nextLine();
+		try (Scanner sc = new Scanner(System.in)) {
+			user = sc.nextLine();
+			System.out.println("Password:");
+			password = sc.nextLine();
+		}
 	}
 	public static void main(String[] args) {
 		login();
+		setRole();
+		
+		System.out.println("End of main");
 	}
 	public static void setRole() {
-		if (password.equals("pasw0rd"))
-			role = "customer";
+		String truePassword = null;
+		String trueRole = null;
+		try {
+			Connection conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement getPassword = conn.prepareCall("select * from passwords where username = '"+user+"'");
+			ResultSet rs = getPassword.executeQuery();
+			rs.next();
+			truePassword = rs.getString(2);
+			trueRole = rs.getString(3);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (password.equals(truePassword))
+			role = trueRole;
 		else
 			role = null;
 	}
--- src/test/java/com/revature/project0/Project0Test.java
+++ src/test/java/com/revature/project0/Project0Test.java
@@ -23,20 +23,18 @@ class Project0Test {
 	}
 	@Test
 	void goodCustomerPasswordTest() {
-		ByteArrayInputStream in = new ByteArrayInputStream("Testy\npasw0rd".getBytes());
-		System.setIn(in);		
-		Project0.login();
-		Project0.setRole();
+		Project0.user = "Testy";
+		Project0.password = "pasw0rd";
 		
+		Project0.setRole();
 		assertEquals("customer", Project0.role);
 	}
 	@Test
 	void badCustomerPasswordTest() {
-		ByteArrayInputStream in = new ByteArrayInputStream("Testy\npassw0rd".getBytes());
-		System.setIn(in);
-		Project0.login();
-		Project0.setRole();
+		Project0.user = "Testy";
+		Project0.password = "password";
 		
+		Project0.setRole();
 		assertEquals(null, Project0.role);
 	}
 	
