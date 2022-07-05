a06c7eea7c10fc447d33c114013e974c0cd17f31
MichaelEldredge <michaelgeldredge@gmail.com>
2022-07-04 18:31:27 -0600

    Last safe commit before loosing the database connection

--- src/main/java/com/revature/project0/Project0.java
+++ src/main/java/com/revature/project0/Project0.java
@@ -9,27 +9,90 @@ import com.revature.jdbc.utils.ConnectionUtils;
 
 public class Project0 {
 
+	
 	public static String user;
 	public static String password;
 	public static String role;
+	public static Scanner sc;
 
-	public static void login() {
-		System.out.println("Username: ");
-		try (Scanner sc = new Scanner(System.in)) {
-			user = sc.nextLine();
-			System.out.println("Password:");
-			password = sc.nextLine();
-		}
-	}
 	public static void main(String[] args) {
-		login();
-		setRole();
-		
+		sc = new Scanner(System.in);
+		role = null;
+		while(role == null) {
+			login();
+			setRole();
+		}
+		int task = 0;
+		if (role.equals("customer")) {
+			System.out.println("If you would like to apply for a new account enter 1.");
+			System.out.println("If you would like to view an existing account enter 2.");
+			System.out.println("If you would like to make a withdrawal enter 3.");
+			System.out.println("If you would like to make a deposit enter 4.");
+			task = Integer.parseInt(sc.nextLine());
+			switch(task) {
+			case 1: {// new account request
+				System.out.println("What would you like to call your new account?");
+				String accountName = sc.nextLine();
+				System.out.println("How much do you want to deposit?");
+				int deposit = Integer.parseInt(sc.nextLine());
+				applyForNewAccount(accountName, deposit);
+				System.out.println("Account request logged.");
+				break;
+			}
+			case 2: {// view current balance
+				System.out.println("What is the name of your account?");
+				String accountName = sc.nextLine();
+				System.out.println("Your current balance is " + getBalance(accountName));
+				break;
+			}
+			case 3: { // Make a withdrawal
+				System.out.println("Which account would you like to make a withdrawal from?");
+				String accountName = sc.nextLine();
+				System.out.println("How much would you like to like to withdrawal?");
+				int withdrawl = Integer.parseInt(sc.nextLine());
+				addToBalance(accountName,-1*withdrawl);
+				break;
+			}
+			case 4:{ // Make a deposit
+				System.out.println("Which account would you like to make a deposit to?");
+				String accountName = sc.nextLine();
+				System.out.println("How much would you like to like to deposit?");
+				int deposit = Integer.parseInt(sc.nextLine());
+				addToBalance(accountName,deposit);
+				break;
+			}
+			default:
+				System.out.println("Not a real transaction");
+			}
+		if (role.equals("employee")) {
+			System.out.println("PLACEHOLDER TEXT");
+			task = Integer.parseInt(sc.nextLine());
+			switch(task) {
+			case 1:
+				System.out.println("TASK 1");
+				break;
+			default:
+				System.out.println("Not a real task");
+			}
+			
+		}
+			
+		}
+		sc.close();
 		System.out.println("End of main");
 	}
+	
+	public static void login() {
+		System.out.println("Username: ");
+		user = sc.nextLine();
+		System.out.println("Password:");
+		password = sc.nextLine();
+	}
+	
 	public static void setRole() {
 		String truePassword = null;
 		String trueRole = null;
+		String toPrint = null;
 		try {
 			Connection conn = ConnectionUtils.getInstance().getConnection();
 			CallableStatement getPassword = conn.prepareCall("select * from passwords where username = '"+user+"'");
@@ -38,12 +101,74 @@ public class Project0 {
 			truePassword = rs.getString(2);
 			trueRole = rs.getString(3);
 		} catch (SQLException e) {
+			role = null;
+			toPrint = "Incorrect username";
+		}
+		if(toPrint == null) {
+			if (password.equals(truePassword)) {
+				role = trueRole;
+				toPrint = "Login sucessful your role is "+role;
+			}
+			else {
+				role = null;
+				toPrint = "Incorrect password";
+			}
+		}
+		System.out.println(toPrint);
+	}
+
+
+	public static void applyForNewAccount(String accountType, int amount) {
+		try {
+			Connection conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement clearTestyApplication = conn.prepareCall("insert into account_applications values ('"+user+"', '"+accountType+"', '"+amount+"')");
+			clearTestyApplication.executeUpdate();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (password.equals(truePassword))
-			role = trueRole;
-		else
-			role = null;
+
 	}
+	public static int getBalance(String accountName) {
+		int balance = 0;
+		try {
+			Connection conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement getBalance = conn.prepareCall("select balance from accounts where account_owner = '"+user+"' and account_name = '"+accountName+"'");
+			ResultSet rs = getBalance.executeQuery();
+			rs.next();
+			balance = rs.getInt(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return balance;
+	}
+
+	public static void addToBalance(String account, int i) {
+		int newbalance = getBalance(account) + i;
+		try {
+			Connection conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement addToBalance = conn.prepareCall(
+					"update accounts set balance = "+newbalance+" where account_owner = '"+user+"' and account_name = '"+account+"'");
+			addToBalance.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void deleteApplication(String accountOwner, String accountName) {
+		try {
+			Connection conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement delApp = conn.prepareCall("delete from account_applications where customer = 'Boss' and account_name = 'temporary'");
+			delApp.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+			
 }
--- src/test/java/com/revature/project0/Project0Test.java
+++ src/test/java/com/revature/project0/Project0Test.java
@@ -3,11 +3,18 @@ package com.revature.project0;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class Project0Test {
+import com.revature.jdbc.utils.ConnectionUtils;
 
+class Project0Test {
 	@Test
 	void test() {
 		assertTrue(true);
@@ -16,10 +23,23 @@ class Project0Test {
 	void loginTest() {
 		ByteArrayInputStream in = new ByteArrayInputStream("Testy\npasw0rd".getBytes());
 		System.setIn(in);
+		Project0.sc = new Scanner(System.in);
 		Project0.login();
 		
 		assertEquals("Testy",Project0.user);
 		assertEquals("pasw0rd",Project0.password);
+	}
+	@Test
+	void multipleLoginTest() {
+		ByteArrayInputStream in = new ByteArrayInputStream("Bob\n123\nTesty\npasw0rd".getBytes());
+		System.setIn(in);
+		Project0.sc = new Scanner(System.in);
+		Project0.login();
+		Project0.login();
+		
+		assertEquals("Testy",Project0.user);
+		assertEquals("pasw0rd",Project0.password);
+		
 	}
 	@Test
 	void goodCustomerPasswordTest() {
@@ -38,6 +58,82 @@ class Project0Test {
 		assertEquals(null, Project0.role);
 	}
 	
-	
+	@Test
+	void goodEmployeePasswordTest() {
+		Project0.user = "Boss";
+		Project0.password = "Jazz";
+		
+		Project0.setRole();
+		assertEquals("employee", Project0.role);
+	}
+	@Test
+	void badEmployeePasswordTest() {
+		Project0.user = "Boss";
+		Project0.password = "Jazzzz";
+		
+		Project0.setRole();
+		assertEquals(null, Project0.role);
+	}
+	@Test
+	void invalidUserPasswordTest() {
+		Project0.user = "Bob";
+		Project0.password = "";
+		
+		Project0.setRole();
+		assertEquals(null, Project0.role);
+	}
+	@Test
+	void applyForNewAccountTest() {
+		Project0.user = "Testy";
+		String shouldBeTesty = "NotTestyYet";
+		try {
+			Connection conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement clearTestyApplication = conn.prepareCall("delete from account_applications where customer = 'Testy'");
+			clearTestyApplication.executeUpdate();
+			Project0.applyForNewAccount("checking", 100);
+			CallableStatement getAccountApplication = conn.prepareCall("select * from account_applications where customer = 'Testy'");
+			ResultSet rs = getAccountApplication.executeQuery();
+			rs.next();
+			shouldBeTesty = rs.getString(1);
+		} catch (SQLException e) {
+			fail("unable to get correct response from database");
+			e.printStackTrace();
+		}
+		assertEquals("Testy",shouldBeTesty);
+	}
+	@Test
+	void getBalanceTest() {
+		Project0.user = "Testy";
+		int foundBalance = Project0.getBalance("old_account");
+		assertEquals(987,foundBalance);
+	}
+	@Test
+	void addToBalanceTest() {
+		Project0.user = "Testy";
+		int initialBalance = Project0.getBalance("checking");
+		Project0.addToBalance("checking", 1);
+		assertEquals(initialBalance + 1, Project0.getBalance("checking"));
+	}
+	@Test
+	void removeApplicationTest() {
+		Project0.user = "Boss";
+		Project0.applyForNewAccount("temporary", 100);
+		Project0.deleteApplication("Boss", "temporary");
+		Connection conn;
+		int count = 0;
+		
+		try {
+			conn = ConnectionUtils.getInstance().getConnection();
+			CallableStatement getAccountApplication = conn.prepareCall("select * from account_applications where customer = 'Boss' and account_name = 'temporary'");
+			ResultSet rs = getAccountApplication.executeQuery();
+			while(rs.next())
+				count++;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("Connection failed");
+			e.printStackTrace();
+		}
+		assertEquals(0,count);
+	}
 
 }
